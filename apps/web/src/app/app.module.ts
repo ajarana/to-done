00892d21a72ddb390cd0735fc68d727379a54e91
Andres Arana <ajoelarana@gmail.com>
2020-12-11 12:55:15 -0500

    Added angular service worker package to enable PWA features

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, HelloComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
